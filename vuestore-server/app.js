ae81ed46f499d1427c2ab9ba334ef6441727a6b1
fikrimuzadi <fikrimuzadi@gmail.com>
2024-04-18 20:52:53 +0700

    upload shop

---
+++ vuestore-server/app.js
@@ -0,0 +1,44 @@
+const express = require('express')
+
+const app = express()
+
+const path = require('path')
+
+const cors = require('cors')
+
+const PORT = process.env.PORT || 8000
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use('/img', express.static(path.join(__dirname, './public/img')))
+
+const db = require('./app/models')
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        // useFindAndModify: false
+    })
+    .then((result) => {
+        console.log('Database Connected!')
+    }).catch((err) => {
+        console.log("Cannot connect to database!", err)
+        process.exit()
+    })
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Welcome to vuestore-server'
+    })
+})
+
+require('./app/routes/product.route')(app)
+require('./app/routes/order.route')(app)
+
+app.listen(PORT, () => {
+    console.log(`server is runing on http://localhost:${PORT}`)
+})
